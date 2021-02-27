9d0858adf5e7848370eccd0db887834a5ce3eb01
rajkumar-inovage <rajkumar@innovagesoftwares.com>
2021-02-27 19:10:42 +0530

    Signed-off-by: rajkumar-inovage <rajkumar@innovagesoftwares.com>

--- assets/js/datastorm.js
+++ assets/js/datastorm.js
@@ -1,5 +1,39 @@
 (function ($) {
   $("#header").load("header.html");
   $("#footer").load("footer.html");
-  $(document).ready(function () {});
+  $(document).ready(function () {
+    new Swiper(".product-history", {
+      slidesPerView: 1.1,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+
+      coverflow: {
+        rotate: 40,
+        stretch: 0,
+        depth: 50,
+        modifier: 1,
+        slideShadows: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1.1,
+        },
+        768: {
+          slidesPerView: 2.3,
+        },
+        1024: {
+          slidesPerView: 4.2,
+          loop: true,
+        },
+        1920: {
+          slidesPerView: 4.3,
+          loop: true,
+        },
+      },
+    });
+  });
 })(jQuery);
