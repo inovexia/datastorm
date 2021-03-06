effb11672465c49942aae3deaf80249793754bbe
Ankit Shukla <4nkit5hukla@gmail.com>
2021-03-06 19:34:06 +0530

    Fix

--- assets/js/datastorm.js
+++ assets/js/datastorm.js
@@ -1,64 +1,65 @@
-(function ($) {
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
-  $(document).ready(function () {
-    new Swiper(".product-history", {
+;(function($) {
+  $('#header').load('header.html')
+  $('#footer').load('footer.html')
+  $(document).ready(function() {
+    new Swiper('.product-history', {
       slidesPerView: 1.1,
       spaceBetween: 30,
       loop: true,
       autoplay: {
         delay: 3500,
-        disableOnInteraction: false,
+        disableOnInteraction: false
       },
       breakpoints: {
         640: {
-          slidesPerView: 1.1,
+          slidesPerView: 1.1
         },
         768: {
-          slidesPerView: 2.3,
+          slidesPerView: 2.3
         },
         1024: {
           slidesPerView: 4.2,
-          loop: true,
+          loop: true
         },
         1920: {
           slidesPerView: 4.3,
-          loop: true,
-        },
-      },
-    });
-    new Swiper(".product-history-2", {
+          loop: true
+        }
+      }
+    })
+    new Swiper('.product-history-2', {
       slidesPerView: 1.1,
       spaceBetween: 30,
       loop: true,
       autoplay: {
         delay: 3500,
-        disableOnInteraction: false,
+        disableOnInteraction: false
       },
       scrollbar: {
-        el: ".swiper-scrollbar",
-        hide: true,
+        el: '.swiper-scrollbar',
+        hide: true
       },
       breakpoints: {
         640: {
-          slidesPerView: 1.1,
+          slidesPerView: 1.1
         },
         768: {
-          slidesPerView: 2.3,
+          slidesPerView: 2.3
         },
         1024: {
           slidesPerView: 4.2,
-          loop: true,
+          loop: true
         },
         1920: {
           slidesPerView: 4.3,
-          loop: true,
-        },
-      },
-    });
-    $(".continue").click(function () {
-      $(".default-stage").fadeOut();
-      $(".stage-2").fadeIn();
-    });
-  });
-})(jQuery);
+          loop: true
+        }
+      }
+    })
+    $('.process .continue').click(function() {
+      $('.default-stage, .process .continue').fadeOut(function() {
+        $('.stage-2, .process .form-submit').fadeIn()
+      })
+    })
+  })
+})(jQuery)
