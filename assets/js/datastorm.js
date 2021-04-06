69e4c8062446fc828070d835cd4be1bde936772f
raj-inovexia <rajkumar@innovagesoftwares.com>
2021-04-06 11:57:13 +0530

    Updated

--- assets/js/datastorm.js
+++ assets/js/datastorm.js
@@ -56,6 +56,58 @@
         },
       },
     });
+    new Swiper(".service-slider", {
+      slidesPerView: 1.1,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1.1,
+        },
+        768: {
+          slidesPerView: 2.3,
+        },
+        1024: {
+          slidesPerView: 3.5,
+          spaceBetween: 50,
+          loop: true,
+        },
+      },
+    });
+    new Swiper(".partners-list-slider", {
+      slidesPerView: 1.1,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1.1,
+        },
+        768: {
+          slidesPerView: 2.3,
+        },
+        1024: {
+          slidesPerView: 3.5,
+          spaceBetween: 50,
+          loop: true,
+        },
+      },
+    });
     $(".process .continue").click(function () {
       $(".default-stage, .process .continue").fadeOut(function () {
         $(".stage-2, .process .form-submit").fadeIn();
@@ -79,9 +131,9 @@
         }, 500);
       });
     });
-    let snapScroll = $("header, section, footer").SnapScroll({
-      hashes: true,
-    });
+    // let snapScroll = $("header, section, footer").SnapScroll({
+    //   hashes: true,
+    // });
 
     //Listen for active element change
     //You could listen globally like $(document)
@@ -89,9 +141,77 @@
 
     //Listen for visible element change
     //You could listen on a specific element like $("header")
-    $(document).on(
-      snapScroll.eventChangeVisible,
-      function (evt, visibleList) {}
-    );
+
+    // $(document).on(
+    //   snapScroll.eventChangeVisible,
+    //   function (evt, visibleList) {}
+    // );
   });
 })(jQuery);
+
+$(document).scroll(function () {
+  $("section").each(function () {
+    if (
+      $(this).position().top <= $(document).scrollTop() + 350 &&
+      $(this).position().top + $(this).outerHeight() > $(document).scrollTop()
+    ) {
+      let dark = $(this).attr("id");
+      if (dark == "section4") {
+        $("body").addClass("dark-mode");
+      } else {
+        $("body").removeClass("dark-mode");
+      }
+    }
+  });
+});
+
+$(document).scroll(function () {
+  $("section").each(function () {
+    if (
+      $(this).position().top <= $(document).scrollTop() &&
+      $(this).position().top + $(this).outerHeight() > $(document).scrollTop()
+    ) {
+      let dark = $(this).attr("id");
+      if (dark == "section5") {
+        $(".our-past-projects").addClass("pos-fixed");
+      } else {
+        $(".our-past-projects").removeClass("pos-fixed");
+      }
+    }
+  });
+});
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $("#header").outerHeight();
+
+$(window).scroll(function (event) {
+  didScroll = true;
+});
+
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  var st = $(this).scrollTop();
+  // if (Math.abs(lastScrollTop - st) <= delta) return;
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $("#header").removeClass("nav-down").addClass("nav-up");
+  } else if (st <= 0) {
+    $("#header").removeClass("nav-down");
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $("#header").removeClass("nav-up").addClass("nav-down");
+    }
+  }
+
+  lastScrollTop = st;
+}
